6f403a217aed1cad519a79da021d6d3d1568fa29
ttrubinov <t.trubinov@g.nsu.ru>
2022-09-26 09:16:38 +0700

    W

--- task_1_1_1_1/src/main/java/ru/nsu/fit/trubinov/Main.java
+++ task_1_1_1_1/src/main/java/ru/nsu/fit/trubinov/Main.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the length of the array");
+        System.out.println("Enter the length of the array: ");
         int len = scan.nextInt();
         int[] arr = new int[len];
-        System.out.println("Enter the array");
+        System.out.println("Enter the array: ");
         for (int i = 0; i < len; i++) {
             arr[i] = scan.nextInt();
         }
