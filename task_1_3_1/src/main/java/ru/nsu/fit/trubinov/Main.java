df985187973376d36e96270caacfd71ceb5dd254
ttrubinov <t.trubinov@g.nsu.ru>
2022-11-14 00:33:16 +0700

    Working substring finder

--- task_1_3_1/src/main/java/ru/nsu/fit/trubinov/Main.java
+++ task_1_3_1/src/main/java/ru/nsu/fit/trubinov/Main.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File f = new File("C:\\Users\\TTrubinov\\IdeaProjects\\oop\\task_1_3_1\\src\\main\\java\\ru\\nsu\\fit\\trubinov\\input.txt");
-        Substring s = new Substring(f, "ag");
-        s.SubstringFinder();
+        File f = new File("./task_1_3_1/src/main/java/ru/nsu/fit/trubinov/input.txt");
+        Substring s = new Substring(f, "aa");
+        System.out.println(s.SubstringFinder());
     }
 }
