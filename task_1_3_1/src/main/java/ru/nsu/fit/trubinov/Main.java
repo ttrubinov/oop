933720d7401c16f8668182631f87075eea485f20
ttrubinov <t.trubinov@g.nsu.ru>
2022-11-14 17:31:51 +0700

    Rebasing

--- task_1_3_1/src/main/java/ru/nsu/fit/trubinov/Main.java
+++ task_1_3_1/src/main/java/ru/nsu/fit/trubinov/Main.java
@@ -1,12 +1,15 @@
 package ru.nsu.fit.trubinov;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         File f = new File("./task_1_3_1/src/main/java/ru/nsu/fit/trubinov/input.txt");
-        Substring s = new Substring(f, "aa");
-        System.out.println(s.SubstringFinder());
+        InputStream stream = new FileInputStream(f);
+        SubstringFinderInStream s = new Aho_Corasick();
+        System.out.println(s.find(stream, ("aaaaaaaaaaaaaaaaaa").toCharArray()));
     }
 }
