f33cc4640ea6c4c83291299dc1b496545a6dc3ad
ttrubinov <t.trubinov@g.nsu.ru>
2022-11-19 20:25:52 +0700

    Javadocs

--- task_1_3_1/src/main/java/ru/nsu/fit/trubinov/KnuthMorrisPratt.java
+++ task_1_3_1/src/main/java/ru/nsu/fit/trubinov/KnuthMorrisPratt.java
@@ -7,11 +7,29 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+/**
+ * Knuth-Morris-Pratt algorithm of finding all occurrences
+ * of a substring in a stream, using prefix-function.
+ * Symbol '#' cannot be used in a stream, because
+ * this algorithm requires to add one character to the end
+ * of a substring, which will never be used in the main string.
+ * Reads characters from a stream one by one, so memory consumption
+ * doesn't exceed 2 * length of a substring.
+ */
 public class KnuthMorrisPratt implements SubstringFinderInStream {
     private BufferedReader reader;
     private char[] s;
     private ArrayList<Integer> prefix;
 
+    /**
+     * Find indices of all occurrences of an input substring
+     * in an input stream by Knuth-Morris-Pratt algorithm.
+     *
+     * @param input     input stream
+     * @param substring substring to find in the stream
+     * @return indices of all occurrences of the substring
+     * @throws IOException if something went wrong with reading the stream
+     */
     @Override
     public ArrayList<Integer> find(InputStream input, char[] substring) throws IOException {
         init(input, substring);
@@ -27,8 +45,8 @@ public class KnuthMorrisPratt implements SubstringFinderInStream {
             if (prefix.get(pos) == 0 && prefix.get(pos - 1) == 0) {
                 Iterator<Integer> iterator = prefix.listIterator(s.length);
                 for (int i = s.length; i <= pos; i++) {
-                    iterator.next();
-                    iterator.remove();
+                    iterator.next();              // deleting useless prefixes
+                    iterator.remove();            // to reduce memory consumption
                 }
                 pos = s.length - 1;
             }
@@ -52,9 +70,12 @@ public class KnuthMorrisPratt implements SubstringFinderInStream {
 
     private void prefix_function(char c, int i) {
         int j = prefix.get(i - 1);
-        while (j > 0 && c != s[j])
+        while (j > 0 && c != s[j]) {
             j = prefix.get(j - 1);
-        if (c == s[j]) j++;
+        }
+        if (c == s[j]) {
+            j++;
+        }
         prefix.add(j);
     }
 }
--- task_1_3_1/src/main/java/ru/nsu/fit/trubinov/SubstringFinderInStream.java
+++ task_1_3_1/src/main/java/ru/nsu/fit/trubinov/SubstringFinderInStream.java
@@ -4,6 +4,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
+/**
+ * Find all occurrences of a substring in a stream.
+ */
 public interface SubstringFinderInStream {
+    /**
+     * Find indices of all occurrences of an input substring
+     * in an input stream.
+     *
+     * @param input     input stream
+     * @param substring substring to find in the stream
+     * @return indices of all occurrences of the substring
+     * @throws IOException if something went wrong with reading the stream
+     */
     ArrayList<Integer> find(InputStream input, char[] substring) throws IOException;
 }
