31ff9d586b59b8a559a4d899e6d6c4a4b885df38
ttrubinov <ttrubinov@users.noreply.github.com>
2022-11-20 09:55:39 +0000

    Deploying to gh-pages from @ ttrubinov/oop@c96859f95793e6a1fe6d55af44e05d456b48bc0b 🚀

---
+++ task_1_4_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.fit.trubinov","l":"RecordBook"},{"p":"ru.nsu.fit.trubinov","l":"RecordBook.Semester"},{"p":"ru.nsu.fit.trubinov","l":"RecordBook.Student"},{"p":"ru.nsu.fit.trubinov","l":"RecordBook.Subject"}]
