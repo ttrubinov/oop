0cc0f08d4de5c659e4921deedf57926cbd158004
ttrubinov <t.trubinov@g.nsu.ru>
2022-12-06 22:02:25 +0700

    toString, Javadocs, Tests, record

--- task_1_4_1/src/main/java/ru/nsu/fit/trubinov/RecordBook.java
+++ task_1_4_1/src/main/java/ru/nsu/fit/trubinov/RecordBook.java
@@ -3,21 +3,57 @@ package ru.nsu.fit.trubinov;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-// TODO: Tests, Javadocs
+import static java.lang.Integer.max;
 
-@SuppressWarnings("unused")
+/**
+ * Record book class, which can calculate average mark in a semester and in all semesters,
+ * can show will you have increased scholarship and red diploma.
+ */
 public class RecordBook {
-    public double averageMark(Student student) {
-        double res = 0;
-        for (Semester i : student.semesters) {
-            res += i.averageMarkInSemester();
-        }
-        return res / student.semesters.size();
+    private final ArrayList<Semester> semesters;
+    public String studentName;
+
+    /**
+     * Constructor of record book class, put your name to create your record book.
+     *
+     * @param name name of a student
+     */
+    public RecordBook(String name) {
+        studentName = name;
+        this.semesters = new ArrayList<>();
     }
 
+    /**
+     * Add a semester to the record book.
+     *
+     * @param semester semester to add into the record book.
+     */
+    public void addSemester(Semester semester) {
+        semesters.add(semester);
+    }
+
+    /**
+     * Calculates average mark for all semesters.
+     *
+     * @return average mark
+     */
+    public double averageMark() {
+        double res = 0;
+        for (Semester i : semesters) {
+            res += i.averageMarkInSemester();
+        }
+        return res / semesters.size();
+    }
+
+    /**
+     * Calculates will you have increased scholarship in this semester.
+     *
+     * @param semester semester, scholarship in which needs to be calculated
+     * @return will you have increased scholarship
+     */
     public boolean isScholarshipIncreased(Semester semester) {
         int cnt4 = 0;
-        for (var i : semester.marks.keySet()) {
+        for (Subject i : semester.marks.keySet()) {
             if (semester.marks.get(i) == 4) {
                 cnt4++;
             }
@@ -28,23 +64,113 @@ public class RecordBook {
         return true;
     }
 
-    public static class Student {
-        public String name;
-        public ArrayList<Semester> semesters = new ArrayList<>();
-
-        public void addSemester(Semester semester) {
-            semesters.add(semester);
+    /**
+     * Calculates will you get red diploma after graduating.
+     *
+     * @return will you get red diploma
+     */
+    public boolean isDiplomaRed() {
+        int cnt4 = 0;
+        int cnt5 = 0;
+        for (Semester semester : semesters) {
+            for (Subject subject : semester.marks.keySet()) {
+                if (semester.marks.get(subject) == 3) {
+                    return false;
+                }
+                if (semester.marks.get(subject) == 4) {
+                    cnt4++;
+                }
+                if (semester.marks.get(subject) == 5) {
+                    cnt5++;
+                }
+            }
         }
+        return ((double) cnt4) / (cnt4 + cnt5) < 0.25;
     }
 
-    public static class Subject {
-        public String subjectName;
-        public String teacherName;
+    /**
+     * Converts the record book into human-readable string format.
+     *
+     * @return record book in string format
+     */
+    public String toString() {
+        String tmp = "Record book of " + studentName + "\n";
+        int maxSize = tmp.length();
+
+        for (Semester semester : semesters) {
+            for (Subject subject : semester.marks.keySet()) {
+                tmp = subject.teacherName + " |  = " + subject.subjectName
+                        + semester.marks.get(subject) + "\n";
+                maxSize = max(maxSize, tmp.length());
+            }
+        }
+        StringBuilder s = new StringBuilder("\n" + "=".repeat(maxSize + 3) + "\n");
+        int sLen = (maxSize - ("Record book of " + studentName).length());
+        int sLen1;
+        if (sLen % 2 == 0) {
+            sLen /= 2;
+            sLen1 = sLen;
+        } else {
+            sLen /= 2;
+            sLen1 = sLen + 1;
+        }
+        s.append("| ").append(" ".repeat(sLen)).append("Record book of ")
+                .append(studentName).append(" ".repeat(sLen1)).append("|\n");
+        s.append("|").append("=".repeat(maxSize + 1)).append("|\n");
+        int cnt = 1;
+        for (Semester semester : semesters) {
+            sLen = (maxSize - ("Semester " + cnt).length());
+            if (sLen % 2 == 0) {
+                sLen /= 2;
+                sLen1 = sLen;
+            } else {
+                sLen /= 2;
+                sLen1 = sLen + 1;
+            }
+            if (cnt > 1) {
+                s.append("|").append("=".repeat(maxSize + 1)).append("|\n");
+            }
+            s.append("| ").append(" ".repeat(sLen)).append("Semester ")
+                    .append(cnt).append(" ".repeat(sLen1)).append("|\n");
+            s.append("|").append("=".repeat(maxSize + 1)).append("|\n");
+            for (Subject subject : semester.marks.keySet()) {
+                sLen = (maxSize - (subject.teacherName + " |  = "
+                        + subject.subjectName + semester.marks.get(subject)).length());
+                if (sLen % 2 == 0) {
+                    sLen /= 2;
+                    sLen1 = sLen;
+                } else {
+                    sLen /= 2;
+                    sLen1 = sLen + 1;
+                }
+                s.append("| ").append(" ".repeat(sLen)).append(subject.subjectName)
+                        .append(" = ").append(semester.marks.get(subject)).append(" | ")
+                        .append(subject.teacherName).append(" ".repeat(sLen1)).append("|\n");
+            }
+            cnt++;
+        }
+        s.append("=".repeat(maxSize + 3)).append("\n");
+        return s.toString();
     }
 
-    public static class Semester {
-        public HashMap<Subject, Integer> marks = new HashMap<>();
-        int semesterId;
+    /**
+     * Subject class.
+     *
+     * @param subjectName name of the subject
+     * @param teacherName name of a teacher of the subject
+     */
+    public record Subject(String subjectName, String teacherName) {
+
+    }
+
+    /**
+     * Semester class, which can calculate average mark in this semester.
+     * You can add new subject to the semester.
+     *
+     * @param marks      marks in all subjects
+     * @param semesterId id of this semester.
+     */
+    public record Semester(HashMap<Subject, Integer> marks, int semesterId) {
 
         public void addSubject(Subject subject, int mark) {
             marks.put(subject, mark);
--- task_1_4_1/src/test/java/RecordBookTest.java
+++ task_1_4_1/src/test/java/RecordBookTest.java
@@ -1,31 +1,49 @@
 import org.junit.jupiter.api.Test;
 import ru.nsu.fit.trubinov.RecordBook;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RecordBookTest {
     @Test
     void testRecordBook() {
-        RecordBook a = new RecordBook();
-        RecordBook.Student Petya = new RecordBook.Student();
-        Petya.name = "Petya";
-        RecordBook.Subject math = new RecordBook.Subject();
-        math.subjectName = "Math";
-        math.teacherName = "TeacherName";
-        RecordBook.Subject history = new RecordBook.Subject();
-        history.subjectName = "History";
-        history.teacherName = "TeacherName";
-        RecordBook.Subject physics = new RecordBook.Subject();
-        physics.subjectName = "Physics";
-        physics.teacherName = "TeacherName";
-        RecordBook.Semester semester1 = new RecordBook.Semester();
+        RecordBook a = new RecordBook("Petya");
+        RecordBook.Subject math = new RecordBook.Subject("Math", "MathTeacherName");
+        RecordBook.Subject history = new RecordBook.Subject("History", "HistoryTeacherName");
+        RecordBook.Subject physics = new RecordBook.Subject("Physics", "PhysicsTeacherName");
+        RecordBook.Semester semester1 = new RecordBook.Semester(new HashMap<>(), 0);
         semester1.addSubject(math, 4);
         semester1.addSubject(history, 5);
         semester1.addSubject(physics, 5);
-        Petya.addSemester(semester1);
+        a.addSemester(semester1);
         assertEquals(semester1.averageMarkInSemester(), 14. / 3);
-        assertEquals(a.averageMark(Petya), 14. / 3);
+        assertEquals(a.averageMark(), 14. / 3);
         assertTrue(a.isScholarshipIncreased(semester1));
+        assertFalse(a.isDiplomaRed());
+        RecordBook.Semester semester2 = new RecordBook.Semester(new HashMap<>(), 1);
+        semester2.addSubject(math, 5);
+        semester2.addSubject(history, 5);
+        semester2.addSubject(physics, 5);
+        a.addSemester(semester2);
+        assertEquals(semester2.averageMarkInSemester(), 5);
+        assertEquals(a.averageMark(), 4.833333333333334);
+        assertTrue(a.isScholarshipIncreased(semester2));
+        assertTrue(a.isDiplomaRed());
+        // ====================================
+        // |       Record book of Petya       |
+        // |==================================|
+        // |            Semester 1            |
+        // |==================================|
+        // | Physics = 5 | PhysicsTeacherName |
+        System.out.println(a);  // |    Math = 4 | MathTeacherName    |
+        // | History = 5 | HistoryTeacherName |
+        // |==================================|
+        // |            Semester 2            |
+        // |==================================|
+        // | Physics = 5 | PhysicsTeacherName |
+        // |    Math = 5 | MathTeacherName    |
+        // | History = 5 | HistoryTeacherName |
+        // ====================================
     }
 }
