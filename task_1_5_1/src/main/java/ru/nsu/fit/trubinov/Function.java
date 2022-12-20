d29edaa0a296a18f4a5908bd7dd902d11c69d487
ttrubinov <t.trubinov@g.nsu.ru>
2022-12-20 16:03:41 +0700

    Number

---
+++ task_1_5_1/src/main/java/ru/nsu/fit/trubinov/Function.java
@@ -0,0 +1,6 @@
+package ru.nsu.fit.trubinov;
+
+public class Function<T> {
+    public String name;
+    public int arity;
+}
---
+++ task_1_5_1/src/main/java/ru/nsu/fit/trubinov/Parser.java
@@ -0,0 +1,9 @@
+package ru.nsu.fit.trubinov;
+
+import java.util.Scanner;
+
+public class Parser {
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
+    
+}
