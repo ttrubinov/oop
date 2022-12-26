72810467aba59a2cd64e63a78e0cf7c81f00e516
ttrubinov <t.trubinov@g.nsu.ru>
2022-12-26 18:15:41 +0700

    Better tests

--- task_1_3_1/src/test/java/SubstringTest.java
+++ task_1_3_1/src/test/java/SubstringTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import ru.nsu.fit.trubinov.AhoCorasick;
 import ru.nsu.fit.trubinov.KnuthMorrisPratt;
@@ -13,15 +14,25 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class SubstringTest {
-    public static Stream<SubstringFinderInStream> findingAlgorithms() {
-        return Stream.of(new AhoCorasick(), new KnuthMorrisPratt());
+    public static Stream<Arguments> findingAlgorithms() {
+        return Stream.of(
+                arguments(new AhoCorasick(), "aaaaaa", List.of(1, 2)),
+                arguments(new AhoCorasick(), "b", List.of(0)),
+                arguments(new AhoCorasick(), "g", List.of(8)),
+                arguments(new AhoCorasick(), "a", List.of(1, 2, 3, 4, 5, 6, 7)),
+                arguments(new KnuthMorrisPratt(), "aaaaaa", List.of(1, 2)),
+                arguments(new KnuthMorrisPratt(), "b", List.of(0)),
+                arguments(new KnuthMorrisPratt(), "g", List.of(8)),
+                arguments(new KnuthMorrisPratt(), "a", List.of(1, 2, 3, 4, 5, 6, 7)));
     }
+
 
     @ParameterizedTest
     @MethodSource("findingAlgorithms")
-    void finderTest(SubstringFinderInStream s) throws IOException {
+    void finderTest(SubstringFinderInStream s, String pattern, List<Integer> a) throws IOException {
         File f = new File("./src/main/java/ru/nsu/fit/trubinov/input.txt");
         File stringToFind = new File("./src/main/java/ru/nsu/fit/trubinov/input2.txt");
         InputStream stream = new FileInputStream(f);
@@ -29,12 +40,6 @@ public class SubstringTest {
         assertEquals(s.find(stream, c), List.of(5984668));
         f = new File("./src/main/java/ru/nsu/fit/trubinov/input3.txt");
         stream = new FileInputStream(f);
-        assertEquals(s.find(stream, "aaaaaa"), List.of(1, 2));
-        stream = new FileInputStream(f);
-        assertEquals(s.find(stream, "a"), List.of(1, 2, 3, 4, 5, 6, 7));
-        stream = new FileInputStream(f);
-        assertEquals(s.find(stream, "b"), List.of(0));
-        stream = new FileInputStream(f);
-        assertEquals(s.find(stream, "g"), List.of(8));
+        assertEquals(s.find(stream, pattern), a);
     }
 }
