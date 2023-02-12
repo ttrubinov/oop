13a2f2382d0e3686b289050dd88b2b793cdc947f
ttrubinov <ttrubinov@users.noreply.github.com>
2023-02-12 10:07:29 +0000

    Deploying to gh-pages from @ ttrubinov/oop@0465bdb893534ec4cee0ba52ea45d169422f8c61 🚀

--- task_2_1_1/package-search-index.js
+++ task_2_1_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.fit.trubinov"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.fit.trubinov.primalityChecking"}]
