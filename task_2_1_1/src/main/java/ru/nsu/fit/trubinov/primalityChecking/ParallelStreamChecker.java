0cc6fc1fde5a4fe68ca8f867288979e46745ba75
ttrubinov <t.trubinov@g.nsu.ru>
2023-02-22 13:19:18 +0700

    ForEach to noneMatch

--- task_2_1_1/src/main/java/ru/nsu/fit/trubinov/primalityChecking/ParallelStreamChecker.java
+++ task_2_1_1/src/main/java/ru/nsu/fit/trubinov/primalityChecking/ParallelStreamChecker.java
@@ -1,27 +1,10 @@
 package ru.nsu.fit.trubinov.primalityChecking;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.Arrays;
 
 public class ParallelStreamChecker implements ArrayPrimalityChecker {
     @Override
     public boolean isArrayPrime(long[] arr) {
-        AtomicBoolean returnVal = new AtomicBoolean(false);
-        List<Long> list = new ArrayList<>(arr.length);
-        for (long n : arr) {
-            list.add(n);
-        }
-        Runnable doNothing = () -> {
-        };
-        list.parallelStream().takeWhile(number -> {
-            if (isComposite(number)) {
-                returnVal.set(true);
-                return false;
-            } else {
-                return true;
-            }
-        }).forEach(x -> doNothing.run());
-        return !returnVal.get();
+        return Arrays.stream(arr).boxed().toList().parallelStream().noneMatch(this::isComposite);
     }
 }
