4603037b577abcee51420aca5c6ad61cefba9694
ttrubinov <t.trubinov@g.nsu.ru>
2023-03-03 13:01:42 +0700

    Parametrized tests

--- task_2_1_1/src/test/java/ru/nsu/fit/trubinov/PrimesTest.java
+++ task_2_1_1/src/test/java/ru/nsu/fit/trubinov/PrimesTest.java
@@ -1,7 +1,9 @@
 package ru.nsu.fit.trubinov;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import ru.nsu.fit.trubinov.primalityChecking.ArrayPrimalityChecker;
 import ru.nsu.fit.trubinov.primalityChecking.MultithreadingChecker;
 import ru.nsu.fit.trubinov.primalityChecking.ParallelStreamChecker;
@@ -10,19 +12,24 @@ import ru.nsu.fit.trubinov.primalityChecking.SequentialChecker;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PrimesTest {
     static long[] primes;
     static long[] primesWithComposite;
     static long[] randomNumbers;
 
+    public static Stream<ArrayPrimalityChecker> primalityCheckingAlgorithms() {
+        return Stream.of(new SequentialChecker(), new MultithreadingChecker(1),
+                new MultithreadingChecker(4), new MultithreadingChecker(), new ParallelStreamChecker());
+    }
+
     @BeforeAll
     public static void setUp() throws FileNotFoundException {
-        int n = 100000;
         String path = "./src/test/java/ru/nsu/fit/trubinov/testData/";
+        int n = 10000;
         Scanner scanner = new Scanner(new File(path + "primes.txt"));
         primes = new long[n];
         for (int i = 0; i < n; i++) {
@@ -40,34 +47,11 @@ public class PrimesTest {
         }
     }
 
-    @Test
-    public void SequentialTest() throws InterruptedException {
-        ArrayPrimalityChecker s = new SequentialChecker();
-        assertTrue(s.isArrayPrime(primes));
-        assertFalse(s.isArrayPrime(primesWithComposite));
-        assertFalse(s.isArrayPrime(randomNumbers));
-    }
 
-    @Test
-    public void MultithreadingChecker4Test() throws InterruptedException {
-        ArrayPrimalityChecker s = new MultithreadingChecker(4);
-        assertTrue(s.isArrayPrime(primes));
-        assertFalse(s.isArrayPrime(primesWithComposite));
-        assertFalse(s.isArrayPrime(randomNumbers));
-    }
-
-    @Test
-    public void MultithreadingChecker12Test() throws InterruptedException {
-        ArrayPrimalityChecker s = new MultithreadingChecker();
-        assertTrue(s.isArrayPrime(primes));
-        assertFalse(s.isArrayPrime(primesWithComposite));
-        assertFalse(s.isArrayPrime(randomNumbers));
-    }
-
-    @Test
-    public void ParallelTest() throws InterruptedException {
-        ArrayPrimalityChecker s = new ParallelStreamChecker();
-        assertTrue(s.isArrayPrime(primes));
+    @ParameterizedTest
+    @MethodSource("primalityCheckingAlgorithms")
+    void finderTest(ArrayPrimalityChecker s) throws InterruptedException {
+        Assertions.assertTrue(s.isArrayPrime(primes));
         assertFalse(s.isArrayPrime(primesWithComposite));
         assertFalse(s.isArrayPrime(randomNumbers));
     }
