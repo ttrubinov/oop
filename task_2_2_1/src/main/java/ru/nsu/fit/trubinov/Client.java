2b5ffdafb95a32c4dbdccfec796fb1102c1e7a6b
ttrubinov <t.trubinov@g.nsu.ru>
2023-03-12 18:41:10 +0700

    Client runnable implementation

--- task_2_2_1/src/main/java/ru/nsu/fit/trubinov/Client.java
+++ task_2_2_1/src/main/java/ru/nsu/fit/trubinov/Client.java
@@ -1,8 +1,32 @@
 package ru.nsu.fit.trubinov;
 
-public class Client {
+import java.util.concurrent.ThreadLocalRandom;
 
-    public void accept(Pizza t) {
+public class Client implements Runnable {
+    private final Orders orders;
+    private int minTimeBetweenOrders;
+    private int maxTimeBetweenOrders;
 
+    public Client(Orders orders) {
+        this.orders = orders;
+    }
+
+    public void order(Pizza t) {
+        synchronized (orders) {
+            orders.add(t);
+            orders.notifyAll();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                wait(ThreadLocalRandom.current().nextInt(minTimeBetweenOrders, maxTimeBetweenOrders + 1));
+                order(new Pizza());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
