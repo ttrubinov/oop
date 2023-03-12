b59bb877c1d6be01b8d1e6e2adcb2bb985193c00
ttrubinov <t.trubinov@g.nsu.ru>
2023-03-12 18:40:43 +0700

    Baker runnable implementation

--- task_2_2_1/src/main/java/ru/nsu/fit/trubinov/Baker.java
+++ task_2_2_1/src/main/java/ru/nsu/fit/trubinov/Baker.java
@@ -1,7 +1,14 @@
 package ru.nsu.fit.trubinov;
 
-public class Baker {
+public class Baker implements Runnable {
     private int cookingTime;
+    private final Orders orders;
+    private final Storage storage;
+
+    public Baker(Orders orders, Storage storage) {
+        this.orders = orders;
+        this.storage = storage;
+    }
 
     public Pizza get() {
         try {
@@ -10,5 +17,29 @@ public class Baker {
             throw new RuntimeException(e);
         }
         return new Pizza();
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                synchronized (orders) {
+                    while (orders.isEmpty()) {
+                        wait();
+                    }
+                    orders.take();
+                    orders.notifyAll();
+                }
+                synchronized (storage) {
+                    while (storage.isFull()) {
+                        wait();
+                    }
+                    storage.add(get());
+                    storage.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
