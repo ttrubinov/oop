a88bc88da655089a34e28e3709a37ea5b4322eef
ttrubinov <t.trubinov@g.nsu.ru>
2023-03-21 10:08:02 +0700

    More info

--- task_2_2_1/src/main/java/ru/nsu/fit/trubinov/Storage.java
+++ task_2_2_1/src/main/java/ru/nsu/fit/trubinov/Storage.java
@@ -5,14 +5,22 @@ import java.util.Queue;
 
 public class Storage {
     private final Queue<Pizza> pizzas = new LinkedList<>();
-    private int maxSize;
+    private final int maxSize;
+
+    public Storage(int maxSize) {
+        this.maxSize = maxSize;
+    }
 
     public boolean isFull() {
-        return !pizzas.isEmpty();
+        return pizzas.size() == maxSize;
     }
 
     public boolean isEmpty() {
         return pizzas.isEmpty();
+    }
+
+    public int size() {
+        return pizzas.size();
     }
 
     public void add(Pizza pizza) {
