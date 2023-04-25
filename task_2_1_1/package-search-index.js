87a1aded12ecb96d312aa8aa975f0747f789ab25
molo4evan <molo4evan@users.noreply.github.com>
2023-04-25 06:51:59 +0000

    Deploying to gh-pages from @ ttrubinov/oop@9a11538c544af2964fae1902bbcab19dbfbd9942 🚀

--- task_2_1_1/package-search-index.js
+++ task_2_1_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.fit.trubinov.primalityChecking"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.fit.trubinov.primalityChecking"}];updateSearchResults();
