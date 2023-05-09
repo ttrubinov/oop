c08700474220dd235d5a5afc1c4960f62396279b
ttrubinov <t.trubinov@g.nsu.ru>
2023-05-09 18:21:48 +0700

    Field

---
+++ task_2_3_1/src/main/java/ru/nsu/fit/trubinov/model/Model.java
@@ -0,0 +1,184 @@
+package ru.nsu.fit.trubinov.model;
+
+import ru.nsu.fit.trubinov.model.field.Direction;
+import ru.nsu.fit.trubinov.model.field.Field;
+import ru.nsu.fit.trubinov.model.fieldObjects.Apple;
+import ru.nsu.fit.trubinov.model.fieldObjects.Wall;
+import ru.nsu.fit.trubinov.model.fieldObjects.snake.BotSnake;
+import ru.nsu.fit.trubinov.model.fieldObjects.snake.Snake;
+import ru.nsu.fit.trubinov.utils.Coordinates;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Model {
+    private final Snake userSnake;
+    private final List<BotSnake> botSnakes;
+    private final List<Snake> allSnakes;
+    private final List<Wall> walls;
+    private final List<Apple> apples;
+    private final Field field;
+    private final int difficultyLevel;
+    private final int botsCount;
+
+    public Model(int width, int height, int difficultyLevel) {
+        this.difficultyLevel = difficultyLevel;
+        botsCount = difficultyLevel;
+        field = new Field(width, height);
+        userSnake = new Snake(new Coordinates(width / 2, height / 2), field, Direction.RIGHT);
+        botSnakes = new ArrayList<>();
+        spawnBots();
+        allSnakes = new ArrayList<>();
+        allSnakes.add(userSnake);
+        allSnakes.addAll(botSnakes);
+        walls = new ArrayList<>();
+        apples = new ArrayList<>();
+    }
+
+    public Coordinates getMaxCoordinates() {
+        return field.getWidthAndHeight();
+    }
+
+    public Snake getUserSnake() {
+        return userSnake;
+    }
+
+    public List<BotSnake> getBotSnakes() {
+        return botSnakes;
+    }
+
+    public List<Apple> getApples() {
+        return apples;
+    }
+
+    public List<Wall> getWalls() {
+        return walls;
+    }
+
+    public Coordinates makeMove() {
+        collectApples(userSnake);
+        moveBots();
+        Coordinates intersection = deathIntersectionCoordinates(userSnake);
+        userSnake.move();
+        if (intersection != null) {
+            return intersection;
+        }
+        allSnakes.removeIf(snake -> snake.length <= 0);
+        spawnWalls();
+        spawnApples();
+        spawnBots();
+        System.out.println(field);
+        return null;
+    }
+
+    private void moveBots() {
+        List<BotSnake> botsToDelete = new ArrayList<>();
+        Iterator<BotSnake> iterator = botSnakes.iterator();
+        while (iterator.hasNext()) {
+            BotSnake botSnake = iterator.next();
+            botSnake.getNewDirection();
+            Coordinates botIntersection = deathIntersectionCoordinates(botSnake);
+            collectApples(botSnake);
+            botSnake.move();
+            if (botIntersection != null) {
+                botSnake.getCoordinates().forEach(field::addEmptyCell);
+                field.addFieldObjectWithCollision(botSnake.getHead());
+                iterator.remove();
+                botsToDelete.add(botSnake);
+            }
+        }
+        botSnakes.removeAll(botsToDelete);
+        allSnakes.removeAll(botsToDelete);
+    }
+
+    private void spawnBots() {
+        if ((double) botSnakes.size() >= botsCount) {
+            return;
+        }
+        List<Coordinates> emptyCells = field.getAllEmptyCells(2);
+        while ((double) botSnakes.size() < botsCount && emptyCells.size() > 0) {
+            botSnakes.add(new BotSnake(emptyCells.get(0), field));
+            field.addFieldObjectWithCollision(emptyCells.get(0));
+            emptyCells.remove(0);
+            emptyCells = field.getAllEmptyCells(2);
+        }
+    }
+
+    private void collectApples(Snake snake) {
+        if (field.intersectionWithApple(snake)) {
+            snake.length++;
+        }
+        apples.stream().filter(apple ->
+                snake.getNextHeadPosition().equals(apple.coordinates())).findFirst().ifPresent(apples::remove);
+    }
+
+    private Coordinates deathIntersectionCoordinates(Snake snake) {
+        return field.deathIntersectionCoordinates(snake);
+    }
+
+    private void spawnApples() {
+        if ((double) apples.size() >= Math.sqrt(field.size()) / (difficultyLevel)) {
+            return;
+        }
+        List<Coordinates> emptyCells = field.getAllEmptyCells(0);
+        while ((double) apples.size() < Math.sqrt(field.size()) / (difficultyLevel) && emptyCells.size() > 0) {
+            if (!field.isEmpty(emptyCells.get(0))) {
+                continue;
+            }
+            apples.add(new Apple(emptyCells.get(0)));
+            field.addNoCollisionFieldObject(emptyCells.get(0));
+            emptyCells.remove(0);
+            emptyCells = field.getAllEmptyCells(0);
+        }
+    }
+
+    private void spawnWalls() {
+        int maxDifficultyLevel = 11;
+        int neededWallsSize = field.size() / 10 / (maxDifficultyLevel - difficultyLevel);
+        if (walls.size() >= neededWallsSize) {
+            return;
+        }
+        List<Coordinates> emptyCells = field.getAllEmptyCells(1);
+        while (walls.size() < neededWallsSize && emptyCells.size() > 0) {
+            int wallSize = ThreadLocalRandom.current().nextInt(1, 3 + 1);
+            List<Coordinates> wallCoordinates = new ArrayList<>();
+            for (int i = 0; i < wallSize; i++) {
+                int randomCord = ThreadLocalRandom.current().nextInt(1, 9 + 1);
+                wallCoordinates.add(new Coordinates(emptyCells.get(0).X() - 1 + randomCord % 3,
+                        emptyCells.get(0).Y() - 1 + randomCord / 3));
+
+                field.addFieldObjectWithCollision(wallCoordinates.get(i));
+            }
+            walls.add(new Wall(wallCoordinates));
+            emptyCells.remove(0);
+            emptyCells = field.getAllEmptyCells(1);
+        }
+    }
+
+    /**
+     * Check if snake can do a turn in the direction (snake can't turn 180°).
+     *
+     * @param snake     Snakes that turns
+     * @param direction Direction to turn
+     * @return true if snake can do a turn in the direction
+     */
+    public boolean isPossibleTurn(Snake snake, Direction direction) {
+        return snake.isPossibleTurn(direction);
+    }
+
+    public Coordinates resize(int width, int height) {
+        field.resize(width, height);
+        for (Coordinates coordinates : userSnake.getCoordinates()) {
+            if (coordinates.X() >= width - 1 || coordinates.Y() >= height - 1) {
+                return coordinates;
+            }
+        }
+        botSnakes.removeIf(botSnake -> !botSnake.isInbounds(width, height));
+        allSnakes.removeIf(snake -> !snake.isInbounds(width, height));
+        walls.removeIf(wall -> !wall.isInbounds(width, height));
+        apples.removeIf(apple -> !apple.isInbounds(width, height));
+        return null;
+    }
+}
---
+++ task_2_3_1/src/main/java/ru/nsu/fit/trubinov/model/field/Field.java
@@ -0,0 +1,163 @@
+package ru.nsu.fit.trubinov.model.field;
+
+import ru.nsu.fit.trubinov.model.fieldObjects.snake.Snake;
+import ru.nsu.fit.trubinov.utils.Coordinates;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Field {
+    private int width;
+    private int height;
+    private int[][] field;
+
+    public Field(int width, int height) {
+        this.width = width;
+        this.height = height;
+        field = new int[width][height];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (i == 0 || i == width - 1 || j == 0 || j == height - 1) {
+                    field[i][j] = 2;
+                } else {
+                    field[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public Coordinates getWidthAndHeight() {
+        return new Coordinates(width, height);
+    }
+
+    public int size() {
+        return width * height;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (int j = 0; j < height; j++) {
+            for (int i = 0; i < width; i++) {
+                s.append(field[i][j]);
+            }
+            s.append('\n');
+        }
+        return String.valueOf(s);
+    }
+
+    public void resize(int width, int height) {
+        Field newField = new Field(width, height);
+        for (int i = 0; i < Math.min(this.width, width); i++) {
+            for (int j = 0; j < Math.min(this.height, height); j++) {
+                newField.field[i][j] = this.field[i][j];
+                if (i == this.width - 1 || j == this.height - 1) {
+                    newField.field[i][j] = 0;
+                }
+            }
+        }
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (i == width - 1 || j == height - 1) {
+                    newField.field[i][j] = 2;
+                }
+            }
+        }
+        this.width = width;
+        this.height = height;
+        this.field = newField.field;
+    }
+
+    /**
+     * Add a field object with collision (like snake or wall) to the field.
+     *
+     * @param coordinates Coordinates of a field object with collision
+     */
+    public void addFieldObjectWithCollision(Coordinates coordinates) {
+        if (coordinates.X() > 0 && coordinates.Y() > 0 && coordinates.X() < width - 1 && coordinates.Y() < height - 1) {
+            field[coordinates.X()][coordinates.Y()] = 2;
+        }
+    }
+
+    /**
+     * Add a field object without collision (apple) to the field.
+     *
+     * @param coordinates Coordinates of a field object without collision
+     */
+    public void addNoCollisionFieldObject(Coordinates coordinates) {
+        if (coordinates.X() > 0 && coordinates.Y() > 0 && coordinates.X() < width - 1 && coordinates.Y() < height - 1) {
+            field[coordinates.X()][coordinates.Y()] = 1;
+        }
+    }
+
+    /**
+     * Add an empty cell to the field.
+     *
+     * @param coordinates Coordinates of an empty cell
+     */
+    public void addEmptyCell(Coordinates coordinates) {
+        if (coordinates.X() > 0 && coordinates.Y() > 0 && coordinates.X() < width - 1 && coordinates.Y() < height - 1) {
+            field[coordinates.X()][coordinates.Y()] = 0;
+        }
+    }
+
+    public Coordinates deathIntersectionCoordinates(Snake snake) {
+        Coordinates coordinates = snake.getNextHeadPosition();
+        addEmptyCell(snake.getCoordinates().get(0)); // Tail moved
+        try {
+            if (field[coordinates.X()][coordinates.Y()] != 0 &&
+                    field[coordinates.X()][coordinates.Y()] != 1) {
+                return coordinates;
+            }
+        } catch (ArrayIndexOutOfBoundsException ignored) {
+            return coordinates;
+        }
+        return null;
+    }
+
+    public boolean isEmpty(Coordinates coordinates) {
+        return field[coordinates.X()][coordinates.Y()] == 0;
+    }
+
+    public boolean intersectionWithApple(Snake snake) {
+        Coordinates coordinates = snake.getNextHeadPosition();
+        try {
+            if (field[coordinates.X()][coordinates.Y()] == 1) {
+                return true;
+            }
+        } catch (ArrayIndexOutOfBoundsException ignored) {
+        }
+        return false;
+    }
+
+    /**
+     * Get all empty cells on the field with empty space around this cell.
+     *
+     * @param emptySpace Empty space around every cell needed
+     * @return List of coordinates of empty cells
+     */
+    public List<Coordinates> getAllEmptyCells(int emptySpace) {
+        List<Coordinates> emptyCells = new ArrayList<>();
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                int cnt = 0;
+                for (int k = -emptySpace; k <= emptySpace; k++) {
+                    for (int l = -emptySpace; l <= emptySpace; l++) {
+                        try {
+                            if (field[i + k][j + l] == 0) {
+                                cnt++;
+                            }
+                        } catch (ArrayIndexOutOfBoundsException ignored) {
+                        }
+                    }
+                }
+                if (cnt == (2 * emptySpace + 1) * (2 * emptySpace + 1)) {
+                    emptyCells.add(new Coordinates(i, j));
+                }
+            }
+        }
+        Collections.shuffle(emptyCells);
+        return emptyCells;
+    }
+}
