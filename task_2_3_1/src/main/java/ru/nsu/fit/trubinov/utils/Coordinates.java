6b2ed6882134882389f6a1a5c03ada3b47548f1e
ttrubinov <t.trubinov@g.nsu.ru>
2023-05-09 18:23:06 +0700

    More operations

--- task_2_3_1/src/main/java/ru/nsu/fit/trubinov/utils/Coordinates.java
+++ task_2_3_1/src/main/java/ru/nsu/fit/trubinov/utils/Coordinates.java
@@ -2,8 +2,57 @@ package ru.nsu.fit.trubinov.utils;
 
 
 public record Coordinates(int X, int Y) {
+    public static int getAngle(Coordinates coords1, Coordinates coords2) {
+        if (coords1.Y == coords2.Y) {
+            if (coords1.X > coords2.X) {
+                return 90;
+            } else return 270;
+        } else {
+            if (coords1.Y > coords2.Y) {
+                return 180;
+            } else return 0;
+        }
+    }
+
+    public static Integer getAngle2(Coordinates coords1, Coordinates coords2, Coordinates coordsMid) {
+        if (coords1.Y == coords2.Y || coords1.X == coords2.X) {
+            return null;
+        }
+        if (coords1.X > coords2.X && coords1.Y < coords2.Y && coordsMid.Y != coords2.Y) {
+            return 0;
+        }
+        if (coords1.X > coords2.X && coords1.Y < coords2.Y) {
+            return 180;
+        }
+        if (coords1.X < coords2.X && coords1.Y < coords2.Y && coordsMid.Y == coords2.Y) {
+            return 270;
+        }
+        if (coords1.X < coords2.X && coords1.Y < coords2.Y) {
+            return 90;
+        }
+        if (coords1.X < coords2.X && coordsMid.Y != coords2.Y) {
+            return 180;
+        }
+        if (coords1.X < coords2.X) {
+            return 0;
+        }
+        if (coordsMid.Y == coords2.Y) {
+            return 90;
+        } else {
+            return 270;
+        }
+    }
+
     public Coordinates sum(Coordinates coordinates) {
         return new Coordinates(this.X + coordinates.X, this.Y + coordinates.Y);
+    }
+
+    public Coordinates subtract(Coordinates coordinates) {
+        return new Coordinates(this.X - coordinates.X, this.Y - coordinates.Y);
+    }
+
+    public Coordinates multiply(int n) {
+        return new Coordinates(this.X * n, this.Y * n);
     }
 
     public boolean equals(Coordinates coordinates) {
