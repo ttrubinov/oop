8d2b7c14af6526b9a76dee1cdbbc645d6817f50d
ttrubinov <t.trubinov@g.nsu.ru>
2023-05-09 18:23:50 +0700

    Angle corresponding to direction

--- task_2_3_1/src/main/java/ru/nsu/fit/trubinov/model/field/Direction.java
+++ task_2_3_1/src/main/java/ru/nsu/fit/trubinov/model/field/Direction.java
@@ -11,21 +11,23 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public enum Direction {
-    UP(0, -1, '^', new ArrayList<>(Arrays.asList(KeyType.ArrowUp, 'W', 'w'))),
-    DOWN(0, 1, 'v', new ArrayList<>(Arrays.asList(KeyType.ArrowDown, 'S', 's'))),
-    LEFT(-1, 0, '<', new ArrayList<>(Arrays.asList(KeyType.ArrowLeft, 'A', 'a'))),
-    RIGHT(1, 0, '>', new ArrayList<>(Arrays.asList(KeyType.ArrowRight, 'D', 'd')));
+    UP(0, -1, '^', new ArrayList<>(Arrays.asList(KeyType.ArrowUp, 'W', 'w')), 180),
+    DOWN(0, 1, 'v', new ArrayList<>(Arrays.asList(KeyType.ArrowDown, 'S', 's')), 0),
+    LEFT(-1, 0, '<', new ArrayList<>(Arrays.asList(KeyType.ArrowLeft, 'A', 'a')), 90),
+    RIGHT(1, 0, '>', new ArrayList<>(Arrays.asList(KeyType.ArrowRight, 'D', 'd')), 270);
 
     public final char headCharacter;
+    public final int angle;
     private final int X;
     private final int Y;
     private final List<Object> keys;
 
-    Direction(int X, int Y, char headCharacter, List<Object> keys) {
+    Direction(int X, int Y, char headCharacter, List<Object> keys, int angle) {
         this.X = X;
         this.Y = Y;
         this.headCharacter = headCharacter;
         this.keys = keys;
+        this.angle = angle;
     }
 
     public static Direction getDirectionByKey(KeyStroke keyStroke) {
