3e0235be395445c92709f8043a9660866cb97463
ttrubinov <t.trubinov@g.nsu.ru>
2023-05-19 11:24:24 +0700

    Test with viewer

--- task_2_3_1/src/main/java/ru/nsu/fit/trubinov/presenter/javaFXPresenter/JavaFXPresenter.java
+++ task_2_3_1/src/main/java/ru/nsu/fit/trubinov/presenter/javaFXPresenter/JavaFXPresenter.java
@@ -5,81 +5,61 @@ import javafx.animation.Timeline;
 import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.fxml.FXML;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.SnapshotParameters;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import ru.nsu.fit.trubinov.model.Model;
-import ru.nsu.fit.trubinov.model.field.Direction;
 import ru.nsu.fit.trubinov.model.fieldObjects.Apple;
 import ru.nsu.fit.trubinov.model.fieldObjects.Wall;
+import ru.nsu.fit.trubinov.model.fieldObjects.snake.BotSnake;
 import ru.nsu.fit.trubinov.model.fieldObjects.snake.Snake;
 import ru.nsu.fit.trubinov.presenter.Presenter;
 import ru.nsu.fit.trubinov.utils.Coordinates;
+import ru.nsu.fit.trubinov.utils.Direction;
 import ru.nsu.fit.trubinov.view.javaFXViewer.JavaFXViewer;
+import ru.nsu.fit.trubinov.view.javaFXViewer.Textures;
 
+import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
+import java.util.Map;
 
-
-// TODO: View functions to JavaFXView
 public class JavaFXPresenter implements Presenter {
-    public static final Image snakeHeadImage = new Image(Objects.requireNonNull(
-            JavaFXViewer.class.getResourceAsStream("/textures/snakeHead2.png")));
-    public static final Image snakeTailImage = new Image(Objects.requireNonNull(
-            JavaFXViewer.class.getResourceAsStream("/textures/snakeTail2.png")));
-    public static final Image botSnakeTailImage = new Image(Objects.requireNonNull(
-            JavaFXViewer.class.getResourceAsStream("/textures/botSnakeTail.png")));
-    public static final Image botSnakeHeadImage = new Image(Objects.requireNonNull(
-            JavaFXViewer.class.getResourceAsStream("/textures/botSnakeHead.png")));
-    public static final Image snakeAngledTailImage = new Image(Objects.requireNonNull(
-            JavaFXViewer.class.getResourceAsStream("/textures/snakeAngledTail2.png")));
-    public static final Image snakeEndOfTailImage = new Image(Objects.requireNonNull(
-            JavaFXViewer.class.getResourceAsStream("/textures/snakeEndOfTail.png")));
-    private final Image field1Image = new Image(Objects.requireNonNull(
-            JavaFXViewer.class.getResourceAsStream("/textures/leftField.png")));
-    private final Image field2Image = new Image(Objects.requireNonNull(
-            JavaFXViewer.class.getResourceAsStream("/textures/rightField2.png")));
-    private final Image wallImage = new Image(Objects.requireNonNull(
-            JavaFXViewer.class.getResourceAsStream("/textures/wall.png")));
-    private final Image appleImage = new Image(Objects.requireNonNull(
-            JavaFXViewer.class.getResourceAsStream("/textures/apple.png")),
-            64, 64, false, false);
-    private final Image bedrockImage = new Image(Objects.requireNonNull(
-            JavaFXViewer.class.getResourceAsStream("/textures/bedrock.png")),
-            64, 64, false, false);
-
     private final Stage stage;
+    private final JavaFXViewer viewer;
+    private final Map<KeyCode, Direction> keyMap = Map.of(
+            KeyCode.W, Direction.UP,
+            KeyCode.UP, Direction.UP,
+            KeyCode.A, Direction.LEFT,
+            KeyCode.LEFT, Direction.LEFT,
+            KeyCode.S, Direction.DOWN,
+            KeyCode.DOWN, Direction.DOWN,
+            KeyCode.D, Direction.RIGHT,
+            KeyCode.RIGHT, Direction.RIGHT
+    );
     protected int width;
     protected int height;
     private Model model;
-    private GraphicsContext gc;
-    private Group root;
-    @FXML
-    private Canvas canvas;
-    private int difficultyLevel = 5;
+    private int difficultyLevel = 10;
     private Direction lastDirection;
+    private Map<Snake, Color> snakeColorMap;
 
     public JavaFXPresenter(Stage stage) {
         this.stage = stage;
         width = 512;
         height = 512;
         model = new Model(width / 64, height / 64, difficultyLevel);
-        lastDirection = model.getUserSnake().getDirection();
+        viewer = new JavaFXViewer();
     }
 
     protected void start() {
-        initScene();
-        EventHandler<ActionEvent> eventHandler = null;
-        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(500),
+        viewer.initScene(stage, width, height);
+        init();
+
+//        EventHandler<ActionEvent> eventHandler = null;
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(200),
                 new EventHandler<>() {
 
                     public void handle(ActionEvent event) {
@@ -87,7 +67,8 @@ public class JavaFXPresenter implements Presenter {
                         Coordinates intersection = model.makeMove();
                         draw();
                         if (intersection != null) {
-                            stage.close();
+//                            System.out.println(model.field);
+//                            stage.close();
                         }
                     }
                 }));
@@ -96,30 +77,32 @@ public class JavaFXPresenter implements Presenter {
         timeline.play();
     }
 
+    private void setBotSpawnListener() {
+        model.setSpawnBotsListener(this::addNewBot);
+    }
+
+    public void addNewBot(BotSnake botSnake) {
+        snakeColorMap.put(botSnake, SnakeColor.getRandomSnakeColor().color);
+    }
+
     private void draw() {
         drawField();
         drawSnake(model.getUserSnake());
-        model.getBotSnakes().forEach(this::drawSnake);
+        model.getBotSnakes().forEach(this::drawBotSnake);
         drawWalls(model.getWalls());
         drawApples(model.getApples());
         drawGrid();
     }
 
 
-    public void initScene() {
-        root = new Group();
-        stage.setTitle("Best Snake game");
-        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icon.png"))));
-        canvas = new Canvas(width, height);
-        stage.setResizable(true);
-        gc = canvas.getGraphicsContext2D();
-        root.getChildren().add(canvas);
-        Scene scene = new Scene(root);
-        scene.setOnKeyPressed(this::onKeyPressed);
-        stage.setScene(scene);
-        setResizeListener();
-        stage.show();
+    public void init() {
+        draw();
+        viewer.getScene().setOnKeyPressed(this::onKeyPressed);
         model = new Model(width / 64, height / 64, difficultyLevel);
+        setResizeListener();
+        snakeColorMap = new HashMap<>();
+        model.getBotSnakes().forEach(botSnake -> snakeColorMap.put(botSnake, SnakeColor.getRandomSnakeColor().color));
+        setBotSpawnListener();
     }
 
     private void onKeyPressed(KeyEvent keyEvent) {
@@ -130,54 +113,69 @@ public class JavaFXPresenter implements Presenter {
     }
 
     private Direction getDirectionByKeyEvent(KeyEvent keyEvent) {
-        switch (keyEvent.getCode()) {
-            case UP, W -> {
-                if (model.getUserSnake().isPossibleTurn(Direction.UP)) {
-                    return Direction.UP;
-                }
-            }
-            case DOWN, S -> {
-                if (model.getUserSnake().isPossibleTurn(Direction.DOWN)) {
-                    return Direction.DOWN;
-                }
-            }
-            case LEFT, A -> {
-                if (model.getUserSnake().isPossibleTurn(Direction.LEFT)) {
-                    return Direction.LEFT;
-                }
-            }
-            case RIGHT, D -> {
-                if (model.getUserSnake().isPossibleTurn(Direction.RIGHT)) {
-                    return Direction.RIGHT;
-                }
-            }
-        }
-        return null;
+        return keyMap.get(keyEvent.getCode());
     }
 
     private void changeDirection() {
-        model.getUserSnake().setDirection(lastDirection);
+        if (lastDirection != null) {
+            model.getUserSnake().setDirection(lastDirection);
+        }
     }
 
     private void drawField() {
         for (int i = 0; i < width; i += 64) {
             for (int j = 0; j < height; j += 64) {
                 if (i >= width - width % 64 || j >= height - height % 64) {
-                    gc.drawImage(bedrockImage, i, j);
+                    drawImage(Textures.BEDROCK.getImage(), new Coordinates(i, j));
                 } else {
-                    if ((i + j) % 128 == 0) gc.drawImage(field1Image, i, j);
-                    else gc.drawImage(field2Image, i, j);
+                    if ((i + j) % (2 * 64) == 0) {
+                        drawImage(Textures.FIELD1.getImage(), new Coordinates(i, j));
+                    } else {
+                        drawImage(Textures.FIELD2.getImage(), new Coordinates(i, j));
+                    }
                 }
             }
         }
     }
 
     private void drawSnake(Snake snake) {
-        gc.drawImage(getRotatedSnake(snake.getDirection().angle), snake.getHead().X() * 64, snake.getHead().Y() * 64);
+        drawRotatedImage(Textures.HEAD.getImage(), snake.getDirection().angle, snake.getHead().multiply(64));
         if (snake.getCoordinates().size() > 1) {
-            gc.drawImage(getRotatedImage(snake.getSnakeCeilDirection(snake.getCoordinates().get(1)).angle,
-                            snakeEndOfTailImage),
-                    snake.getCoordinates().get(0).X() * 64, snake.getCoordinates().get(0).Y() * 64);
+            drawRotatedImage(Textures.TAIL_END.getImage(),
+                    snake.getSnakeCeilDirection(snake.getCoordinates().get(1)).angle,
+                    snake.getCoordinates().get(0).multiply(64));
+        }
+        for (int i = 1; i < snake.getCoordinates().size() - 1; i++) {
+            Coordinates coordinates = snake.getCoordinates().get(i);
+            int fstAngle;
+            int sndAngle;
+            fstAngle = snake.getSnakeCeilDirection(coordinates).angle;
+            sndAngle = snake.getSnakeCeilDirection(snake.getCoordinates().get(i + 1)).angle;
+
+            if (fstAngle == sndAngle) {
+                drawRotatedImage(Textures.TAIL.getImage(),
+                        snake.getSnakeCeilDirection(coordinates).angle, coordinates.multiply(64));
+                continue;
+            }
+            if ((fstAngle == 180 && sndAngle == 270) || (fstAngle == 90 && sndAngle == 0)) {
+                drawImage(Textures.ANGLED_TAIL.getImage(), coordinates.multiply(64));
+            } else if ((fstAngle == 270 && sndAngle == 0) || (fstAngle == 180 && sndAngle == 90)) {
+                drawRotatedImage(Textures.ANGLED_TAIL.getImage(), 90, coordinates.multiply(64));
+            } else if ((fstAngle == 0 && sndAngle == 90) || (fstAngle == 270 && sndAngle == 180)) {
+                drawRotatedImage(Textures.ANGLED_TAIL.getImage(), 180, coordinates.multiply(64));
+            } else if ((fstAngle == 90 && sndAngle == 180) || (fstAngle == 0 && sndAngle == 270)) {
+                drawRotatedImage(Textures.ANGLED_TAIL.getImage(), 270, coordinates.multiply(64));
+            }
+        }
+    }
+
+    private void drawBotSnake(Snake snake) {
+        drawRotatedColoredImage(Textures.HEAD.getImage(), snake.getDirection().angle,
+                snakeColorMap.get(snake), snake.getHead().multiply(64));
+        if (snake.getCoordinates().size() > 1) {
+            drawRotatedColoredImage(Textures.TAIL_END.getImage(),
+                    snake.getSnakeCeilDirection(snake.getCoordinates().get(1)).angle,
+                    snakeColorMap.get(snake), snake.getCoordinates().get(0).multiply(64));
         }
         for (int i = 1; i < snake.getCoordinates().size() - 1; i++) {
             Coordinates coordinates = snake.getCoordinates().get(i);
@@ -186,21 +184,23 @@ public class JavaFXPresenter implements Presenter {
             fstAngle = snake.getSnakeCeilDirection(coordinates).angle;
             sndAngle = snake.getSnakeCeilDirection(snake.getCoordinates().get(i + 1)).angle;
             if (fstAngle == sndAngle) {
-                gc.drawImage(getRotatedImage(snake.getSnakeCeilDirection(coordinates).angle, snakeTailImage),
-                        coordinates.X() * 64, coordinates.Y() * 64);
+                drawRotatedColoredImage(Textures.TAIL.getImage(), snake.getSnakeCeilDirection(coordinates).angle,
+                        snakeColorMap.get(snake), coordinates.multiply(64));
                 continue;
             }
             if ((fstAngle == 180 && sndAngle == 270) || (fstAngle == 90 && sndAngle == 0)) {
-                gc.drawImage(snakeAngledTailImage, coordinates.X() * 64, coordinates.Y() * 64);
+                drawColoredImage(Textures.ANGLED_TAIL.getImage(),
+                        snakeColorMap.get(snake), coordinates.multiply(64));
             } else if ((fstAngle == 270 && sndAngle == 0) || (fstAngle == 180 && sndAngle == 90)) {
-                gc.drawImage(getRotatedImage(90, snakeAngledTailImage),
-                        coordinates.X() * 64, coordinates.Y() * 64);
+                drawRotatedColoredImage(Textures.ANGLED_TAIL.getImage(), 90,
+                        snakeColorMap.get(snake), coordinates.multiply(64));
             } else if ((fstAngle == 0 && sndAngle == 90) || (fstAngle == 270 && sndAngle == 180)) {
-                gc.drawImage(getRotatedImage(180, snakeAngledTailImage),
-                        coordinates.X() * 64, coordinates.Y() * 64);
+                drawRotatedColoredImage(Textures.ANGLED_TAIL.getImage(), 180,
+                        snakeColorMap.get(snake), coordinates.multiply(64));
+
             } else if ((fstAngle == 90 && sndAngle == 180) || (fstAngle == 0 && sndAngle == 270)) {
-                gc.drawImage(getRotatedImage(270, snakeAngledTailImage),
-                        coordinates.X() * 64, coordinates.Y() * 64);
+                drawRotatedColoredImage(Textures.ANGLED_TAIL.getImage(), 270,
+                        snakeColorMap.get(snake), coordinates.multiply(64));
             }
         }
     }
@@ -208,7 +208,7 @@ public class JavaFXPresenter implements Presenter {
     private void drawWalls(List<Wall> walls) {
         for (Wall wall : walls) {
             for (Coordinates coordinates : wall.getCoordinates()) {
-                gc.drawImage(wallImage, coordinates.X() * 64, coordinates.Y() * 64);
+                drawImage(Textures.WALL.getImage(), coordinates.multiply(64));
             }
         }
     }
@@ -216,7 +216,7 @@ public class JavaFXPresenter implements Presenter {
     private void drawApples(List<Apple> apples) {
         for (Apple apple : apples) {
             for (Coordinates coordinates : apple.getCoordinates()) {
-                gc.drawImage(appleImage, coordinates.X() * 64, coordinates.Y() * 64);
+                drawImage(Textures.APPLE.getImage(), coordinates.multiply(64));
             }
         }
     }
@@ -225,26 +225,10 @@ public class JavaFXPresenter implements Presenter {
         for (int i = 0; i < width / 64; i++) {
             for (int j = 0; j < height / 64; j++) {
                 if (i == 0 || i == width / 64 - 1 || j == 0 || j == height / 64 - 1) {
-                    gc.drawImage(wallImage, i * 64, j * 64);
+                    drawImage(Textures.WALL.getImage(), new Coordinates(i, j).multiply(64));
                 }
             }
         }
-    }
-
-    private Image getRotatedSnake(int angle) {
-        ImageView imageView = new ImageView(snakeHeadImage);
-        imageView.setRotate(angle);
-        SnapshotParameters params = new SnapshotParameters();
-        params.setFill(Color.TRANSPARENT);
-        return imageView.snapshot(params, null);
-    }
-
-    private Image getRotatedImage(int angle, Image image) {
-        ImageView imageView = new ImageView(image);
-        imageView.setRotate(angle);
-        SnapshotParameters params = new SnapshotParameters();
-        params.setFill(Color.TRANSPARENT);
-        return imageView.snapshot(params, null);
     }
 
     private void setResizeListener() {
@@ -252,13 +236,42 @@ public class JavaFXPresenter implements Presenter {
             width = (int) stage.getWidth();
             height = (int) stage.getHeight();
             model.resize(width / 64, height / 64);
-            root.resize(width, height);
-            canvas = new Canvas(width, height);
-            gc = canvas.getGraphicsContext2D();
-            root.getChildren().set(0, canvas);
+            viewer.resize(width, height);
             draw();
         };
         stage.widthProperty().addListener(stageSizeListener);
         stage.heightProperty().addListener(stageSizeListener);
+    }
+
+    public void drawImage(Image image, Coordinates coordinates) {
+        viewer.gc.drawImage(image, coordinates.X(), coordinates.Y());
+    }
+
+    public void drawRotatedImage(Image image, int angle, Coordinates coordinates) {
+//        ImageView imageView = new ImageView(image);
+//        imageView.setRotate(angle);
+//        SnapshotParameters params = new SnapshotParameters();
+//        params.setFill(Color.TRANSPARENT);
+//        viewer.gc.drawImage(imageView.snapshot(params, null), coordinates.X(), coordinates.Y());
+        viewer.gc.drawImage(image, coordinates.X(), coordinates.Y());
+    }
+
+    public void drawColoredImage(Image image, Color color, Coordinates coordinates) {
+//        ImageView imageView = new ImageView(image);
+//        imageView.setEffect(new ColorAdjust(color.getHue() / 180 - 1, 0, 0, 0.2));
+//        SnapshotParameters params = new SnapshotParameters();
+//        params.setFill(Color.TRANSPARENT);
+//        viewer.gc.drawImage(imageView.snapshot(params, null), coordinates.X(), coordinates.Y());
+        viewer.gc.drawImage(image, coordinates.X(), coordinates.Y());
+    }
+
+    public void drawRotatedColoredImage(Image image, int angle, Color color, Coordinates coordinates) {
+//        ImageView imageView = new ImageView(image);
+//        imageView.setRotate(angle);
+//        imageView.setEffect(new ColorAdjust(color.getHue() / 180 - 1, 0, 0, 0.2));
+//        SnapshotParameters params = new SnapshotParameters();
+//        params.setFill(Color.TRANSPARENT);
+//        viewer.gc.drawImage(imageView.snapshot(params, null), coordinates.X(), coordinates.Y());
+        viewer.gc.drawImage(image, coordinates.X(), coordinates.Y());
     }
 }
--- task_2_3_1/src/main/java/ru/nsu/fit/trubinov/view/javaFXViewer/JavaFXViewer.java
+++ task_2_3_1/src/main/java/ru/nsu/fit/trubinov/view/javaFXViewer/JavaFXViewer.java
@@ -1,5 +1,80 @@
 package ru.nsu.fit.trubinov.view.javaFXViewer;
 
-public class JavaFXViewer {
+import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import ru.nsu.fit.trubinov.utils.Coordinates;
 
+import java.util.Objects;
+
+public class JavaFXViewer {
+    public GraphicsContext gc;
+    private Scene scene;
+    private Group root;
+    @FXML
+    private Canvas canvas;
+
+    public JavaFXViewer() {
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+
+    public void initScene(Stage stage, int width, int height) {
+        root = new Group();
+        stage.setTitle("Best Snake game");
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icon.png"))));
+        canvas = new Canvas(width, height);
+        stage.setResizable(true);
+        gc = canvas.getGraphicsContext2D();
+        root.getChildren().add(canvas);
+        this.scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void resize(int width, int height) {
+        root.resize(width, height);
+        canvas = new Canvas(width, height);
+        gc = canvas.getGraphicsContext2D();
+        root.getChildren().set(0, canvas);
+    }
+
+    public void drawImage(Image image, Coordinates coordinates) {
+        gc.drawImage(image, coordinates.X(), coordinates.Y());
+    }
+
+    public void drawRotatedImage(Image image, int angle, Coordinates coordinates) {
+        ImageView imageView = new ImageView(image);
+        imageView.setRotate(angle);
+        SnapshotParameters params = new SnapshotParameters();
+        params.setFill(Color.TRANSPARENT);
+        gc.drawImage(imageView.snapshot(params, null), coordinates.X(), coordinates.Y());
+    }
+
+    public void drawColoredImage(Image image, Color color, Coordinates coordinates) {
+        ImageView imageView = new ImageView(image);
+        imageView.setEffect(new ColorAdjust(color.getHue() / 180 - 1, 0, 0, 0.2));
+        SnapshotParameters params = new SnapshotParameters();
+        params.setFill(Color.TRANSPARENT);
+        gc.drawImage(imageView.snapshot(params, null), coordinates.X(), coordinates.Y());
+    }
+
+    public void drawRotatedColoredImage(Image image, int angle, Color color, Coordinates coordinates) {
+        ImageView imageView = new ImageView(image);
+        imageView.setRotate(angle);
+        imageView.setEffect(new ColorAdjust(color.getHue() / 180 - 1, 0, 0, 0.2));
+        SnapshotParameters params = new SnapshotParameters();
+        params.setFill(Color.TRANSPARENT);
+        gc.drawImage(imageView.snapshot(params, null), coordinates.X(), coordinates.Y());
+    }
 }
