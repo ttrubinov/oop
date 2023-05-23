299322abbd8602deea49db699739179b9b794fed
ttrubinov <t.trubinov@g.nsu.ru>
2023-05-23 09:14:35 +0700

    Initializing scene

--- task_2_3_1/src/main/java/ru/nsu/fit/trubinov/presenter/javaFXPresenter/ApplicationStart.java
+++ task_2_3_1/src/main/java/ru/nsu/fit/trubinov/presenter/javaFXPresenter/ApplicationStart.java
@@ -1,7 +1,15 @@
 package ru.nsu.fit.trubinov.presenter.javaFXPresenter;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import ru.nsu.fit.trubinov.presenter.javaFXPresenter.controller.MenuController;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class ApplicationStart extends Application {
     public static void main(String[] args) {
@@ -10,7 +18,22 @@ public class ApplicationStart extends Application {
 
     @Override
     public void start(Stage stage) {
-        JavaFXPresenter presenter = new JavaFXPresenter(stage);
-        presenter.start();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/menu.fxml"));
+        Parent root;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Scene scene = new Scene(root);
+        stage.setMinWidth(768);
+        stage.setMinHeight(512);
+        stage.setTitle("Best Snake game");
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icon.png"))));
+        stage.setScene(scene);
+        MenuController menuController = loader.getController();
+        menuController.initGame();
+        menuController.init(stage);
+        stage.show();
     }
 }
