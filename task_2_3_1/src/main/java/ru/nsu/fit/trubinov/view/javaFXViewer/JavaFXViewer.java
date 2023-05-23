bc8c41457eea2a0bee36f778c0ed629c55be1c00
ttrubinov <t.trubinov@g.nsu.ru>
2023-05-23 09:19:19 +0700

    New colors, borders

--- task_2_3_1/src/main/java/ru/nsu/fit/trubinov/view/javaFXViewer/JavaFXViewer.java
+++ task_2_3_1/src/main/java/ru/nsu/fit/trubinov/view/javaFXViewer/JavaFXViewer.java
@@ -1,8 +1,5 @@
 package ru.nsu.fit.trubinov.view.javaFXViewer;
 
-import javafx.fxml.FXML;
-import javafx.scene.Group;
-import javafx.scene.Scene;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -10,57 +7,41 @@ import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
-import javafx.stage.Stage;
 import ru.nsu.fit.trubinov.utils.Coordinates;
-
-import java.util.Objects;
 
 public class JavaFXViewer {
     public GraphicsContext gc;
-    private Scene scene;
-    private Group root;
-    @FXML
-    private Canvas canvas;
 
-    public JavaFXViewer() {
-    }
-
-    public Scene getScene() {
-        return scene;
-    }
-
-    public void initScene(Stage stage, int width, int height) {
-        root = new Group();
-        stage.setTitle("Best Snake game");
-        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icon.png"))));
-        canvas = new Canvas(width, height);
-        stage.setResizable(true);
+    public JavaFXViewer(Canvas canvas) {
         gc = canvas.getGraphicsContext2D();
-        root.getChildren().add(canvas);
-        this.scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
     }
 
-    public void resize(int width, int height) {
-        root.resize(width, height);
-        canvas = new Canvas(width, height);
-        gc = canvas.getGraphicsContext2D();
-        root.getChildren().set(0, canvas);
+    public void setGraphicsContext(GraphicsContext gc) {
+        this.gc = gc;
     }
 
     public void drawDefaultImage(Image image, Coordinates coordinates) {
         gc.drawImage(image, coordinates.X(), coordinates.Y());
     }
 
-    public void drawImage(Image image, int angle, Color color, Coordinates coordinates) {
+    public void drawImage(Image image, int angle, double hue, Coordinates coordinates) {
         ImageView imageView = new ImageView(image);
         imageView.setRotate(angle);
-        if (color != Color.GREEN) {
-            imageView.setEffect(new ColorAdjust(color.getHue() / 180 - 1, 0, 0, 0.2));
+        if (hue != 0) {
+            imageView.setEffect(new ColorAdjust(hue, 0.1, 0, 0.2));
         }
         SnapshotParameters params = new SnapshotParameters();
         params.setFill(Color.TRANSPARENT);
         gc.drawImage(imageView.snapshot(params, null), coordinates.X(), coordinates.Y());
+    }
+
+    public void drawBedrockBorders(Canvas canvas) {
+        System.out.println(canvas.getWidth() + " " + canvas.getHeight());
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        for (int i = 0; i < canvas.getWidth() / 64; i++) {
+            for (int j = 0; j < canvas.getHeight() / 64; j++) {
+                gc.drawImage(Textures.BEDROCK.image, i * 64, j * 64);
+            }
+        }
     }
 }
