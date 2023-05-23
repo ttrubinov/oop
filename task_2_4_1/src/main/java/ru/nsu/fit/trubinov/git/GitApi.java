063b0ed01b8bf7406a8db862ae80abf9339ec159
ttrubinov <t.trubinov@g.nsu.ru>
2023-05-23 11:47:15 +0700

    New functions

--- task_2_4_1/src/main/java/ru/nsu/fit/trubinov/git/GitApi.java
+++ task_2_4_1/src/main/java/ru/nsu/fit/trubinov/git/GitApi.java
@@ -1,9 +1,11 @@
 package ru.nsu.fit.trubinov.git;
 
+import org.eclipse.jgit.api.CreateBranchCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
 import java.io.File;
+import java.io.IOException;
 
 public class GitApi {
     private static final String githubLinkPrefix = "https://github.com/";
@@ -11,12 +13,32 @@ public class GitApi {
     private static final File saveClonedDirectory = new File(
             "./task_2_4_1/src/main/java/ru/nsu/fit/trubinov/git/cloned");
 
-    public static void cloneRepository(String repositoryName) throws GitAPIException {
+    public static Git cloneRepository(String repositoryName) throws GitAPIException {
         try (Git git = Git.cloneRepository()
                 .setURI(githubLinkPrefix + repositoryName + githubLinkPostfix)
                 .setDirectory(saveClonedDirectory)
                 .setCloneAllBranches(true)
                 .call()) {
+            return git;
         }
+    }
+
+    public static boolean isOnBranch(Git git, String branchName) {
+        try {
+            return git.getRepository()
+                    .getFullBranch()
+                    .equals(branchName);
+        } catch (IOException e) {
+            System.out.println("AAAAAAAA");
+            return false;
+        }
+    }
+
+    public static void checkout(Git git, String branchName) throws GitAPIException {
+        git.checkout()
+                .setName(branchName)
+                .setUpstreamMode(CreateBranchCommand.SetupUpstreamMode.TRACK)
+                .setStartPoint("origin/" + branchName)
+                .call();
     }
 }
