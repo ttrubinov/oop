30a05bafc9d5cf7d87dc3da282d3490b51fb7001
ttrubinov <t.trubinov@g.nsu.ru>
2023-05-23 14:58:41 +0700

    Better enum

--- task_2_3_1/src/main/java/ru/nsu/fit/trubinov/view/javaFXViewer/Textures.java
+++ task_2_3_1/src/main/java/ru/nsu/fit/trubinov/view/javaFXViewer/Textures.java
@@ -5,49 +5,21 @@ import javafx.scene.image.Image;
 import java.util.Objects;
 
 public enum Textures {
-    HEAD(new Image(Objects.requireNonNull(JavaFXViewer.class.getResourceAsStream(
-            TextureName.directory + TextureName.HEAD.name)))),
-    TAIL(new Image(Objects.requireNonNull(JavaFXViewer.class.getResourceAsStream(
-            TextureName.directory + TextureName.TAIL.name)))),
-    ANGLED_TAIL(new Image(Objects.requireNonNull(JavaFXViewer.class.getResourceAsStream(
-            TextureName.directory + TextureName.ANGLED_TAIL.name)))),
-    TAIL_END(new Image(Objects.requireNonNull(JavaFXViewer.class.getResourceAsStream(
-            TextureName.directory + TextureName.TAIL_END.name)))),
-    FIELD1(new Image(Objects.requireNonNull(JavaFXViewer.class.getResourceAsStream(
-            TextureName.directory + TextureName.FIELD1.name)))),
-    FIELD2(new Image(Objects.requireNonNull(JavaFXViewer.class.getResourceAsStream(
-            TextureName.directory + TextureName.FIELD2.name)))),
-    WALL(new Image(Objects.requireNonNull(JavaFXViewer.class.getResourceAsStream(
-            TextureName.directory + TextureName.WALL.name)))),
-    APPLE(new Image(Objects.requireNonNull(JavaFXViewer.class.getResourceAsStream(
-            TextureName.directory + TextureName.APPLE.name)),
-            64, 64, false, false)),
-    BEDROCK(new Image(Objects.requireNonNull(
-            JavaFXViewer.class.getResourceAsStream(
-                    TextureName.directory + TextureName.BEDROCK.name
-            )),
-            64, 64, false, false));
+    HEAD("snakeHead.png"),
+    TAIL("snakeTail.png"),
+    ANGLED_TAIL("snakeAngledTail.png"),
+    TAIL_END("snakeEndOfTail.png"),
+    FIELD1("field1.png"),
+    FIELD2("field2.png"),
+    WALL("wall.png"),
+    APPLE("apple.png"),
+    BEDROCK("bedrock.png");
+    private static final String directory = "/textures/";
     public final Image image;
 
-    Textures(Image image) {
-        this.image = image;
-    }
-
-    private enum TextureName {
-        HEAD("snakeHead.png"),
-        TAIL("snakeTail.png"),
-        ANGLED_TAIL("snakeAngledTail.png"),
-        TAIL_END("snakeEndOfTail.png"),
-        FIELD1("field1.png"),
-        FIELD2("field2.png"),
-        WALL("wall.png"),
-        APPLE("apple.png"),
-        BEDROCK("bedrock.png");
-        private static final String directory = "/textures/";
-        private final String name;
-
-        TextureName(String name) {
-            this.name = name;
-        }
+    Textures(String fileName) {
+        this.image = new Image(Objects.requireNonNull(JavaFXViewer.class.getResourceAsStream(
+                Textures.directory + fileName)),
+                64, 64, false, false);
     }
 }
