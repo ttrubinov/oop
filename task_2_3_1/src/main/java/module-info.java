297027b924b5a04c46a3b8c639ab52844c8a5d9a
ttrubinov <t.trubinov@g.nsu.ru>
2023-05-23 09:20:04 +0700

    New export

--- task_2_3_1/src/main/java/module-info.java
+++ task_2_3_1/src/main/java/module-info.java
@@ -3,9 +3,11 @@ module ru.nsu.fit.trubinov {
     requires javafx.fxml;
     requires com.googlecode.lanterna;
 
+    exports ru.nsu.fit.trubinov.presenter.javaFXPresenter.controller;
     exports ru.nsu.fit.trubinov.presenter.javaFXPresenter;
+    opens ru.nsu.fit.trubinov.presenter.javaFXPresenter to javafx.fxml;
+    opens ru.nsu.fit.trubinov.presenter.javaFXPresenter.controller to javafx.fxml;
     opens ru.nsu.fit.trubinov.view.javaFXViewer to javafx.fxml;
     opens ru.nsu.fit.trubinov.presenter to javafx.fxml;
-    opens ru.nsu.fit.trubinov.presenter.javaFXPresenter to javafx.fxml;
     opens ru.nsu.fit.trubinov.view.consoleViewer to javafx.fxml;
 }
