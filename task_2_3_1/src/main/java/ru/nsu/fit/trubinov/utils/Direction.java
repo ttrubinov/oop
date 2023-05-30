3c8ecda19294cea55d8a9a802e187c8559baa72b
ttrubinov <t.trubinov@g.nsu.ru>
2023-05-30 10:20:28 +0700

    Moved chars from direction enum

--- task_2_3_1/src/main/java/ru/nsu/fit/trubinov/utils/Direction.java
+++ task_2_3_1/src/main/java/ru/nsu/fit/trubinov/utils/Direction.java
@@ -1,44 +1,24 @@
 package ru.nsu.fit.trubinov.utils;
 
-import com.googlecode.lanterna.input.KeyStroke;
-import com.googlecode.lanterna.input.KeyType;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
 public enum Direction {
-    UP(0, -1, '^', new ArrayList<>(Arrays.asList(KeyType.ArrowUp, 'W', 'w')), 180),
-    DOWN(0, 1, 'v', new ArrayList<>(Arrays.asList(KeyType.ArrowDown, 'S', 's')), 0),
-    LEFT(-1, 0, '<', new ArrayList<>(Arrays.asList(KeyType.ArrowLeft, 'A', 'a')), 90),
-    RIGHT(1, 0, '>', new ArrayList<>(Arrays.asList(KeyType.ArrowRight, 'D', 'd')), 270);
+    UP(0, -1, 180),
+    DOWN(0, 1, 0),
+    LEFT(-1, 0, 90),
+    RIGHT(1, 0, 270);
 
-    public final char headCharacter;
     public final int angle;
     private final int X;
     private final int Y;
-    private final List<Object> keys;
 
-    Direction(int X, int Y, char headCharacter, List<Object> keys, int angle) {
+    Direction(int X, int Y, int angle) {
         this.X = X;
         this.Y = Y;
-        this.headCharacter = headCharacter;
-        this.keys = keys;
         this.angle = angle;
-    }
-
-    public static Direction getDirectionByKey(KeyStroke keyStroke) {
-        if (keyStroke == null) {
-            return null;
-        }
-        for (Direction direction : getAllDirectionsRandomly()) {
-            if (direction.keys.contains(keyStroke.getKeyType()) || direction.keys.contains(keyStroke.getCharacter())) {
-                return direction;
-            }
-        }
-        return null;
     }
 
     public static List<Direction> getAllDirectionsRandomly() {
